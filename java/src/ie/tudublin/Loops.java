ad2201b003e6e083362b3a3bc6cd1db509d0c2c4
myusername <myemail>
2022-02-16 10:47:12 +0000

    completed grid of circles, need to offset colour

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -80,13 +80,16 @@ public class Loops extends PApplet {
             case 3:
                 background(0);
                 int circles2 = (int) (mouseX / 20.0f);
-                float d = width / circles2;
+                float d = width / (float)circles2;
                 for (int i = 0; i < circles2; i++) {
-                    noStroke();
-                    float x = map(i, 0, circles2, height, 0);
-                    // float y = map(i, 0, circles2, width, 0);
-                    fill(map(i, 0, circles2, 0, 255), 255, 255);
-                    circle(x, 0, d);
+                    for (int j = 0; j < circles2; j++) {
+                        noStroke();
+                        float x = map(i, 0, circles2 - 1, d / 2.0f, width - (d / 2.0f));
+                        float y = map(j, 0, circles2 - 1, d / 2.0f, width - (d / 2.0f));
+                        float c = map(i, 0, circles2, 0, 255);
+                        fill(c, 255, 255);
+                        circle(x, y, d);
+                    }
                 }
             // map(a,b,c,d,e);
             // a = inputvalue
